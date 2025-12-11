60c73b0891260d07de33b0b0e6413919ab0c7966
Rafinha <rafael408alves@gmail.com>
2025-12-11 10:59:04 -0300

    primeiro commit da aplicação

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config (page_title="sistema Bancário", layout="wide")
+
+st.title("sistema de controle bancário")
+st.write("bem vindo ao sistema. Use o manu lateral")
